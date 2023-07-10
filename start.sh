caf5e9ef0794f7f3abaa1a174f46cd1620bb22a3
Marcin-Fraszczak <kolothomasa@wp.pl>
2023-07-10 13:18:56 +0200

    updated README

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 python manage.py migrate
 python manage.py collectstatic
 gunicorn '_config.wsgi'
