8ac90c415957a2636ce39d704b8f96af7879503c
Marcin-Fraszczak <kolothomasa@wp.pl>
2023-07-10 14:20:19 +0200

    moved chromedriver

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@ sudo apt -y install google-chrome-stable
 #version=$(google-chrome --version)
 
 #wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
+#unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
 sudo chown root:root /usr/bin/chromedriver
 sudo chmod +x /usr/bin/chromedriver
